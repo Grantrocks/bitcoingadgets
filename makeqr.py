ec13448f47499decd4a09e5877413ee7f29ef249
Grantrocks <grantmac12345678911@outlook.com>
2022-03-24 00:45:51 -0400

    added and fixed the wallets features 	deleted:    .breakpoints 	deleted:    .replit 	deleted:    __pycache__/encrypt.cpython-38.pyc 	deleted:    __pycache__/getlucky.cpython-38.pyc 	modified:   __pycache__/getlucky.cpython-39.pyc 	modified:   __pycache__/makeqr.cpython-39.pyc 	deleted:    __pycache__/secure.cpython-38.pyc 	deleted:    getlucky.py 	modified:   main.py 	modified:   makeqr.py 	deleted:    poetry.lock 	deleted:    pyproject.toml 	modified:   requirements.txt 	fastgen.py

--- makeqr.py
+++ makeqr.py
@@ -1,5 +1,4 @@
 import qrcode  
 def gqr(address):
   qr_img = qrcode.make(address)  
-  qr_img.save(f"qrcodes/receive.jpg")
-  print("QR Code Saved In QR Codes")
+  qr_img.save(f"qrcodes/{address}.jpg")
